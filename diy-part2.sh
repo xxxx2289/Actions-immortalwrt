93f8bc4570e893c6df765763bd5dc54963121dd0
xuxin1955 <73999603+xuxin1955@users.noreply.github.com>
2026-03-09 06:43:30 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -52,6 +52,7 @@ git clone --depth=1 https://github.com/xuxin1955/luci-app-usb-gadget package/usb
 
 #temp
 git clone https://github.com/lkiuyu/luci-app-cpu-perf package/luci-app-cpu-perf
+git clone https://github.com/lkiuyu/luci-app-cpu-status package/luci-app-cpu-status
 git clone https://github.com/gSpotx2f/luci-app-cpu-status-mini package/luci-app-cpu-status-mini
 git clone https://github.com/lkiuyu/luci-app-temp-status package/luci-app-temp-status
 git clone https://github.com/lkiuyu/DbusSmsForwardCPlus package/DbusSmsForwardCPlus
