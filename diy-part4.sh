5ddc01eb10e0725e38870498b1d827fd7bec785f
xuxin1955 <73999603+xuxin1955@users.noreply.github.com>
2026-03-09 06:44:41 +0800

    Update diy-part4.sh

--- diy-part4.sh
+++ diy-part4.sh
@@ -12,6 +12,7 @@ git clone --depth 1 https://github.com/vernesong/OpenClash.git OpenClash
 
 # temp
 git clone https://github.com/gSpotx2f/luci-app-cpu-perf package/luci-app-cpu-perf
+git clone https://github.com/lkiuyu/luci-app-cpu-status package/luci-app-cpu-status
 git clone https://github.com/gSpotx2f/luci-app-cpu-status-mini package/luci-app-cpu-status-mini
 git clone https://github.com/gSpotx2f/luci-app-temp-status package/luci-app-temp-status
 
