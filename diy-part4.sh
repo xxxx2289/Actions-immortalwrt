56350c9d4176240b4f5519a73ed5ab7ad4ed5d17
xuxin1955 <73999603+xuxin1955@users.noreply.github.com>
2026-03-15 00:47:01 +0800

    Update diy-part4.sh

--- diy-part4.sh
+++ diy-part4.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+cd openwrt && rm -rf target/linux/generic/backport-6.12
 # turboacc
 # curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh --no-sfe
 curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
@@ -10,5 +10,4 @@ echo 'src-git QModem https://github.com/FUjr/QModem' >> feeds.conf.default
 # OpenClash
 git clone --depth 1 https://github.com/vernesong/OpenClash.git OpenClash
 
-cd openwrt && rm -rf target/linux/generic/backport-6.12
 
