ee7e701b3b4cbc67313ecd470947c7c44dc53a93
xuxin1955 <73999603+xuxin1955@users.noreply.github.com>
2026-04-13 12:15:40 +0800

    Update diy-part4.sh

--- diy-part4.sh
+++ diy-part4.sh
@@ -1,25 +1,5 @@
 #!/bin/bash
 
-
-echo "===== 直接覆盖 feeds.conf.default 为新配置 ====="
-cat > feeds.conf.default << 'EOF'
-src-git packages https://github.com/immortalwrt/packages.git;openwrt-25.12
-src-git luci https://github.com/immortalwrt/luci.git;openwrt-25.12
-src-git routing https://github.com/openwrt/routing.git;openwrt-25.12
-src-git telephony https://github.com/openwrt/telephony.git;openwrt-25.12
-src-git video https://github.com/openwrt/video.git;openwrt-25.12
-src-git qmodem https://github.com/FUjr/QModem.git;main
-src-git kenzo https://github.com/kenzok8/openwrt-packages
-src-git small https://github.com/kenzok8/small
-src-git rtp https://github.com/xuanranran/openwrt-rtp2httpd
-src-git easytier https://github.com/EasyTier/luci-app-easytier.git
-EOF
-
-echo "===== 替换后的 feeds.conf.default 内容 ====="
-cat feeds.conf.default
-
-echo "===== feeds.conf.default 覆盖完成 ====="
-
 # turboacc
 curl -sSL https://raw.githubusercontent.com/mufeng05/turboacc/main/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
 
