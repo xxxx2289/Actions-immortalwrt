61eff3bfa00fde86c00c2d72ba485e8eb16f2f2f
xuxin1955 <73999603+xuxin1955@users.noreply.github.com>
2026-04-21 10:33:12 +0800

    Update diy-part4.sh

--- diy-part4.sh
+++ diy-part4.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
+
 # turboacc
 curl -sSL https://raw.githubusercontent.com/mufeng05/turboacc/main/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
 
