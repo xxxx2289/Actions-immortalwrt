970edb691d113d540bb80a3056f1ad52da923164
xxxx2289 <139218008+xxxx2289@users.noreply.github.com>
2026-04-28 10:59:28 +0800

    更新 diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,32 +1,27 @@
 #!/bin/bash
 #
 
-# Modify default IP
-sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
-
 # Modify default theme
 sed -i 's/luci-theme-material/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
-# temp
-git clone https://github.com/lkiuyu/luci-app-cpu-perf package/luci-app-cpu-perf
-git clone https://github.com/lkiuyu/luci-app-cpu-status package/luci-app-cpu-status
-git clone https://github.com/gSpotx2f/luci-app-cpu-status-mini package/luci-app-cpu-status-mini
-git clone https://github.com/lkiuyu/luci-app-temp-status package/luci-app-temp-status
-
-# DbusSmsForwardCPlus
-git clone https://github.com/lkiuyu/DbusSmsForwardCPlus package/DbusSmsForwardCPlus
-
-# Fix libmbim dependency for split glib2 packages
-if [ -f feeds/packages/libs/libmbim/Makefile ]; then
-  sed -i '/define Package\/libmbim/,/endef/s/^[[:space:]]*DEPENDS:=.*/DEPENDS:=+glib2-core +glib2-gobject +glib2-gio/' feeds/packages/libs/libmbim/Makefile
-fi
-
-# Make sure required glib2 split packages are selected
-for p in glib2-core glib2-gmodule glib2-gobject glib2-gio glib2-gthread libmbim; do
+# Disable MBIM / QMI / ModemManager userspace packages to avoid glib2 split dependency errors
+for p in \
+  libmbim \
+  mbim-utils \
+  libqmi \
+  qmi-utils \
+  libqrtr-glib \
+  modemmanager \
+  modemmanager-rpcd \
+  luci-proto-mbim \
+  luci-proto-qmi \
+  luci-proto-modemmanager \
+  luci-app-modemmanager
+do
   sed -i "/^CONFIG_PACKAGE_${p}=/d" .config
   sed -i "/^# CONFIG_PACKAGE_${p} is not set/d" .config
-  echo "CONFIG_PACKAGE_${p}=y" >> .config
+  echo "# CONFIG_PACKAGE_${p} is not set" >> .config
 done
 
-echo "===== libmbim dependency after patch ====="
-grep -A8 -n "define Package/libmbim" feeds/packages/libs/libmbim/Makefile || true
+echo "===== Disabled modem userspace packages ====="
+grep -E "CONFIG_PACKAGE_(libmbim|mbim-utils|libqmi|qmi-utils|libqrtr-glib|modemmanager|modemmanager-rpcd|luci-proto-mbim|luci-proto-qmi|luci-proto-modemmanager|luci-app-modemmanager)" .config || true
